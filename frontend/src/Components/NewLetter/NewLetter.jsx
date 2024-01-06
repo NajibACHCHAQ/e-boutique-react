fe09922c8365a8976d9a02579d299ef760a4df2e
NajibACHCHAQ <najib.achchaq@gmail.com>
2024-01-06 23:29:39 +0100

    ajout composant

--- frontend/src/Components/NewLetter/NewLetter.jsx
+++ frontend/src/Components/NewLetter/NewLetter.jsx
@@ -3,7 +3,7 @@ import './NewLetter.css'
 export const NewLetter = () => {
   return (
     <div className='newletter'>
-        <h1>Recois Nos Offres Exclusives par Email</h1>
+        <h1>Recois Nos Offres Exclusives !</h1>
         <p>S'abonner à la NewLetter</p>
         <div>
             <input type="email" placeholder=' Votre email...' />
