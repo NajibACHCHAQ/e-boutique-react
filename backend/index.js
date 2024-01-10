562d4cbaec6fec4357b790cc0ec3d65175a1f634
NajibACHCHAQ <najib.achchaq@gmail.com>
2024-01-10 11:14:44 +0100

    ajout des endpoints crud

--- backend/index.js
+++ backend/index.js
@@ -27,8 +27,8 @@ const storage = multer.diskStorage({
     }
 })
 
+// Creation d'une route pour upload les images dans le projet via multer
 const upload = multer({storage:storage})
-// Creating upload for images
 app.use('/images',express.static('upload/images'))
 app.post('/upload', upload.single('product'), (req, res) => {
     res.json({
@@ -36,6 +36,89 @@ app.post('/upload', upload.single('product'), (req, res) => {
         image_url: `http://localhost:${port}/images/${req.file.filename}`
     });
 });
+
+// Schema for creating products
+// Model de création d'un produit
+const Product = mongoose.model("Product",{
+    id:{
+        type:Number,
+        required:true
+    },
+    name:{
+        type:String,
+        required:true,
+    },
+    image:{
+        type:String,
+        required:true,
+    },
+    category:{
+        type:String,
+        required:true,
+    },
+    new_price:{
+        type:Number,
+        required:true,
+    },
+    old_price:{
+        type:Number,
+        required:true
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    },
+    available:{
+        type:Boolean,
+        default:true
+    }
+});
+app.post("/addproduct", async(req,res)=>{
+    let products =await Product.find({});
+    let id;
+    if(products.length>0){
+        let last_product_array = products.slice(-1);
+        let last_product = last_product_array[0];
+        id = last_product.id+1;
+    }
+    else{
+        id = 1;
+    }
+    const product = new Product({
+        id:id,
+        name:req.body.name,
+        image:req.body.image,
+        category:req.body.category,
+        new_price:req.body.new_price,
+        old_price:req.body.old_price,
+
+    });
+    console.log(product);
+    await product.save();
+    console.log('saved');
+    res.json({
+        success:true,
+        name:req.body.name
+    })
+})
+
+// Creating API For deleting product
+// Méthode et rout de suppression d'un produit
+app.delete('/removeproduct', async(req,res)=>{
+    await Product.findOneAndDelete({id:req.body.id});
+    console.log('removed');
+    res.json({
+        success:true,
+        name:req.body.name
+    })
+})
+
+// Creating API for getting all products
+app.get('/allproducts',async(req,res)=>{
+    let products = await Product.find({});
+    console.log("all product fetched");
+    res.send(products);
+})
 
 app.listen(port,(error)=>{
     if(!error){
