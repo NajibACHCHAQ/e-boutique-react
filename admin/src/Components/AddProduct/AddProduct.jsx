88e732c38cf95a11220f69b25b9a85f5e4b9b83d
NajibACHCHAQ <najib.achchaq@gmail.com>
2024-01-10 15:19:33 +0100

    implementation de l'ajout product

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -18,13 +18,55 @@ export const AddProduct = () => {
             ...productDetails, [e.target.name]:e.target.value
         })
     }
-    const Add_Product = async()=>{
-        console.log(productDetails);
-    }
+    const Add_Product = async () => {
+        try {
+            console.log(productDetails);
+            let responseData;
+            let product = productDetails;
+    
+            let formData = new FormData();
+            formData.append('product', image);
+    
+            const response = await fetch('http://localhost:4000/upload', {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                },
+                body: formData,
+            });
+    
+            if (response.ok) {
+                responseData = await response.json();
+    
+                if (responseData.success) {
+                    product.image = responseData.image_url;
+                    console.log(product);
+                    await fetch('http://localhost:4000/addproduct', {
+                        method: 'POST', // Assurez-vous de mettre 'POST' entre guillemets
+                        headers: {
+                            Accept: 'application/json',
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify(product),
+                    }).then((resp) => resp.json()).then((data) => {
+                        data.success ? alert("produit ajouté avec succès") : alert("Erreur lors de l'ajout du produit");
+                    });
+                    
+                } else {
+                    console.error('Erreur lors de la requête :', responseData.error);
+                }
+            } else {
+                console.error('Erreur lors de la requête :', response.statusText);
+            }
+        } catch (error) {
+            console.error('Erreur lors de l\'ajout du produit :', error);
+        }
+    };
+    
   return (
     <div className='addproduct'>
         <div className="addproduct-itemfield">
-            <p>Product title</p>
+            <p>Nom du produit</p>
             <input value={productDetails.name} onChange={changeHandler} type="text" name='name' placeholder='Type here' />
         </div>
         <div className="addproduct-price">
